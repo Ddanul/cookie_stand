818fc88930f4cf5cf9a5130ec9a6f2f97f2f0372
Danul De Leon <danul.deleon@gmail.com>
2018-08-06 13:12:36 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+console.log('js is linked');
+
+var firstAndPike = {
+    name : '1st and Pike',
+    min : 23,
+    max : 65,
+    avg : 6.3
+}
+
+var seaTac = {
+    name : '1st and Pike',
+    min : 3,
+    max : 24,
+    avg : 1.4
+}
